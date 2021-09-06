0958213327cac7a461ba4c1c3c4f042065b83639
Ava <ava.soyeon.kim@gmail.com>
2021-09-06 22:18:12 +0900

    add memo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './header.module.css';
 
-const Header = ({ count, left }) => (
+const Header = memo(({ count, left }) => (
   <div>
     <h1>Shopping List</h1>
     <span className={styles.count}>
       {count - left} / {count} items left
     </span>
   </div>
-);
+));
 
 export default Header;
 <h1>Header</h1>;
